fab105a84c3018c22ac287d52e31961151ca63ed
tsevetelina-stoykova <cveti.stoikova@gmail.com>
2022-10-26 18:14:59 +0300

    fix Blog Detail refresh bug

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -2,25 +2,24 @@ import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getOne } from "../app/blogsSlice";
+import { getAssetsByIds } from "../app/assetsSlice";
+import AssetsItem from '../features/AssetsItem';
 import "./BlogDetails.css"
 
 function BlogDetails() {
 	const { seo_id } = useParams();
 	const dispatch = useDispatch();
 	const post = useSelector((state) => state.blogs.map[seo_id]);
+	const assets = useSelector((state) => state.assets);
+	const blog_missing_ids = post ? post.asset_ids.filter((id)=>!assets.map[id]) : [];
 
 	useEffect(() => {
 		if(!post) dispatch(getOne({seo_id}));
 	}, [dispatch, post, seo_id]);
 
-	const date = new Date(post.scheduled * 1000);
-	const parser = new DOMParser();
-	const bodyHTML = parser.parseFromString(post.body, "text/html");
-	const body = bodyHTML.documentElement.textContent;
-
-	// TODO: Related Assets
-
-	// TODO: Fix error on reload
+	useEffect(() => { 
+		if(blog_missing_ids.length) dispatch(getAssetsByIds({ids:blog_missing_ids})); 
+	}, [blog_missing_ids]);
 
 	return (
 		<>{ post ?
@@ -30,18 +29,22 @@ function BlogDetails() {
 					<div className="post-wrapper">
 						<div className="col-8 post-heading mb-3">
 							<h2 className="pb-1">{post.title}</h2>
-							<span className="post-date pb-1">{date.toLocaleDateString()}</span>
+							{post ? <span className="post-date pb-1">{new Date(post.scheduled * 1000).toLocaleDateString()}</span> : "Loading"}
 						</div>
 						<div className="col-8 mb-3">
-							<img className="post-image" src={post.thumb_url}  alt="Post"/>
+							{post ? <img className="post-image" src={post.thumb_url}  alt="Post"/> : "Loading"}
 						</div>
-						<div className="col-8 mb-3">
-							{body}
+						<div className="col-8 mb-3">							
+							{post ? new DOMParser().parseFromString(post.body, "text/html").documentElement.textContent : "Loading"}
 						</div>
-						</div>
-					<div className="col related-assets justify-content-start">
-						<h3>Related Assets</h3>
 					</div>
+				</div>
+				<div className="row related-assets justify-content-start">
+						<h3>Related Assets</h3>
+						{post.asset_ids.map(id => assets.map[id] ?
+							<AssetsItem key={id} asset={assets.map[id]}/> :
+							<p key={id}>Asset missing: {id}</p>
+						)}
 				</div>
 			</div>
 			)
